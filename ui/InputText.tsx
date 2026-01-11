8dfe41c38a264cf5e0e8a1e5bd5555e2d532f4d8
AlanLima <dev.alanlima@gmail.com>
2026-01-11 20:44:50 -0300

    feat: create input with label and icon

---
+++ ui/InputText.tsx
@@ -0,0 +1,31 @@
+import { Input } from "@/ui/input";
+import { LucideIcon } from "lucide-react";
+import { cn } from "@/lib/utils";
+
+interface InputTextProps extends React.ComponentProps<typeof Input> {
+  label?: string;
+  icon?: LucideIcon;
+}
+
+export function InputText({
+  className,
+  label,
+  icon: Icon,
+  ...props
+}: InputTextProps) {
+  return (
+    <label className={cn("relative block", label && "mt-6")}>
+      {label && (
+        <span className="absolute -top-6 text-sm text-muted-foreground">
+          {label}
+        </span>
+      )}
+
+      {Icon && (
+        <Icon className="absolute left-4 top-1/2 h-5 w-5 -translate-y-1/2 text-primary" />
+      )}
+
+      <Input {...props} className={cn(Icon && "pl-12", className)} />
+    </label>
+  );
+}
