50a6650ffa2475c5fc1bcaa3d2fe7bad6c3dd03a
AlanLima <dev.alanlima@gmail.com>
2026-01-16 22:54:34 -0300

    feat: add error message

--- ui/InputText.tsx
+++ ui/InputText.tsx
@@ -1,31 +1,63 @@
 import { Input } from "@/ui/input";
-import { LucideIcon } from "lucide-react";
+import { LucideIcon, TriangleAlert } from "lucide-react";
 import { cn } from "@/lib/utils";
 
 interface InputTextProps extends React.ComponentProps<typeof Input> {
   label?: string;
   icon?: LucideIcon;
+  errorMessage?: string;
 }
 
 export function InputText({
   className,
   label,
+  errorMessage,
   icon: Icon,
   ...props
 }: InputTextProps) {
   return (
-    <label className={cn("relative block w-full", label && "mt-6")}>
+    <label
+      className={cn(
+        "relative block w-full",
+        label && "mt-6",
+        errorMessage && "mb-6"
+      )}
+    >
       {label && (
-        <span className="absolute -top-6 text-sm text-muted-foreground">
+        <span className="absolute -top-6 left-1 text-sm text-muted-foreground">
           {label}
         </span>
       )}
 
       {Icon && (
-        <Icon className="absolute left-4 top-1/2 h-5 w-5 -translate-y-1/2 text-primary" />
+        <Icon
+          className={cn(
+            "absolute left-4 top-1/2 h-5 w-5 -translate-y-1/2 text-primary",
+            errorMessage && "text-destructive"
+          )}
+        />
       )}
 
-      <Input {...props} className={cn(Icon && "pl-12", className)} />
+      <Input
+        aria-invalid={!!errorMessage}
+        {...props}
+        className={cn(Icon && "pl-12", className)}
+      />
+
+      {errorMessage && (
+        <TriangleAlert
+          className={cn(
+            "absolute right-4 top-1/2 h-5 w-5 -translate-y-1/2 text-primary",
+            errorMessage && "text-destructive"
+          )}
+        />
+      )}
+
+      {errorMessage && (
+        <span className="absolute left-1 text-destructive -bottom-5 text-xs">
+          {errorMessage}
+        </span>
+      )}
     </label>
   );
 }
