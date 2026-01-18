14f0794b77d9134d0b7160641a0c54a808782b17
AlanLima <dev.alanlima@gmail.com>
2026-01-17 22:00:34 -0300

    refactor: remove unnecessary parameter

--- features/schemas/auth/password-recovery/PasswordRecoverySchema.ts
+++ features/schemas/auth/password-recovery/PasswordRecoverySchema.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const passwordRecoverySchema = z.object({
-  email: z.email("Email inválido").min(1, "Email obrigatório"),
+  email: z.email("Email inválido"),
 });
 
 export type PasswordRecovery = z.infer<typeof passwordRecoverySchema>;
