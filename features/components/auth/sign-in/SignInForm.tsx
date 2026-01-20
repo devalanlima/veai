2f4bdb6576fbfd175299f0fed4460b8bd6ba5b9f
AlanLima <dev.alanlima@gmail.com>
2026-01-20 20:36:28 -0300

    feat: implements turnstile captcha

--- features/components/auth/sign-in/SignInForm.tsx
+++ features/components/auth/sign-in/SignInForm.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import {
-  SignInSchema,
+  SignIn,
   signInSchema,
 } from "@/features/schemas/auth/sign-in/SignInSchema";
 import { formatZodErrors } from "@/lib/formatZodErrors";
@@ -9,61 +9,117 @@ import { getAuthErrorMessage } from "@/lib/getAuthErrorMessage";
 import { supabase } from "@/lib/supabase/clients/createClient";
 import { Button } from "@/ui/button";
 import { InputText } from "@/ui/InputText";
+import { Turnstile, TurnstileInstance } from "@marsidev/react-turnstile";
 import { Loader2, Lock, Mail } from "lucide-react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import { DetailedHTMLProps, FormHTMLAttributes, useState } from "react";
+import { DetailedHTMLProps, FormHTMLAttributes, useRef, useState } from "react";
+import { toast } from "sonner";
+
+const siteKey = process.env.NEXT_PUBLIC_TURNSTILE_SITE_KEY;
+if (!siteKey) console.error("Turnstile key not configured");
 
 export default function SignInForm({
   ...props
 }: DetailedHTMLProps<FormHTMLAttributes<HTMLFormElement>, HTMLFormElement>) {
-  const [errors, setErrors] = useState<
-    Partial<Record<keyof SignInSchema, string>>
-  >({});
+  const [errors, setErrors] = useState<Partial<Record<keyof SignIn, string>>>(
+    {},
+  );
+  const [supabaseError, setSupabaseError] = useState<string | null>(null);
 
   const [isLoading, setIsLoading] = useState(false);
 
-  const [supabaseError, setSupabaseError] = useState<string | undefined>();
+  const dataUser = useRef<SignIn | null>(null);
+
+  const $turnstile = useRef<TurnstileInstance | null>(null);
 
   const router = useRouter();
 
+  const validateSchema = (data: SignIn) => {
+    const schemaResult = signInSchema.safeParse(data);
+    return schemaResult;
+  };
+
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    setIsLoading(true);
-    setSupabaseError(undefined);
     e.preventDefault();
+    if (isLoading) return;
+    setIsLoading(true);
     setErrors({});
+    setSupabaseError(null);
 
     const formData = new FormData(e.currentTarget);
 
-    const data: SignInSchema = {
-      email: formData.get("email") as string,
-      password: formData.get("password") as string,
+    const data: SignIn = {
+      email: String(formData.get("email") ?? ""),
+      password: String(formData.get("password") ?? ""),
     };
 
-    const result = signInSchema.safeParse(data);
+    const schemaResult = validateSchema(data);
 
-    if (result.success) {
-      const { data, error } = await supabase.auth.signInWithPassword({
-        email: result.data.email,
-        password: result.data.password,
-      });
-
-      if (error) {
-        setSupabaseError(
-          error.code
-            ? getAuthErrorMessage(error.code)
-            : "Ocorreu um erro inesperado.",
-        );
-      } else if (data.user) {
-        router.push(`/`);
-        return;
-      } else {
-        setSupabaseError("Ocorreu um erro ao criar sua conta.");
-      }
+    if (schemaResult.error) {
+      setErrors(formatZodErrors(schemaResult.error));
+      setIsLoading(false);
     } else {
-      setErrors(formatZodErrors(result.error));
+      dataUser.current = data;
+      $turnstile.current?.execute();
     }
+  };
+
+  const signIn = async (
+    email: string,
+    password: string,
+    captchaToken: string,
+  ) => {
+    const { error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+      options: {
+        captchaToken,
+      },
+    });
+
+    if (error) {
+      return {
+        success: false,
+        message: error.code
+          ? getAuthErrorMessage(error.code)
+          : "Ocorreu um erro inesperado.",
+      };
+    }
+
+    return { success: true, message: null };
+  };
+
+  const resetAuthState = () => {
+    $turnstile.current?.reset();
+    dataUser.current = null;
+    setErrors({});
     setIsLoading(false);
+    setSupabaseError(null);
+  };
+
+  const handleException = (message: string) => {
+    toast.error(message);
+    resetAuthState();
+  };
+
+  const handleOnSuccess = async (captchaToken: string) => {
+    if (dataUser.current) {
+      const { email, password } = dataUser.current;
+      dataUser.current = null;
+
+      const authentication = await signIn(email, password, captchaToken);
+
+      if (authentication.success) {
+        router.push("/");
+      } else {
+        setSupabaseError(authentication.message);
+        $turnstile.current?.reset();
+      }
+      setIsLoading(false);
+    } else {
+      handleException("Ocorreu um erro ao resgatar os dados do formulário.");
+    }
   };
 
   return (
@@ -97,11 +153,10 @@ export default function SignInForm({
       )}
 
       <Button
-        asChild={!isLoading}
         size="lg"
         variant="link"
         className="h-6 underline decoration-1 font-light"
-        disabled={isLoading}
+        asChild
       >
         <Link href="/password-recovery">Esqueceu sua senha?</Link>
       </Button>
@@ -114,6 +169,19 @@ export default function SignInForm({
         <Button size="lg" className="w-full" type="submit">
           Entrar
         </Button>
+      )}
+
+      {siteKey && (
+        <Turnstile
+          siteKey={siteKey}
+          ref={$turnstile}
+          onSuccess={handleOnSuccess}
+          onError={() =>
+            handleException("Erro ao validar captcha, tente novamente.")
+          }
+          onExpire={() => handleException("Captcha expirado, tente novamente.")}
+          options={{ size: "invisible", execution: "execute" }}
+        />
       )}
     </form>
   );
