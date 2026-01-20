82e6321f1266c9431873f07c5899342d790b8cca
AlanLima <dev.alanlima@gmail.com>
2026-01-19 22:27:34 -0300

    style: add animate pulse

--- app/(auth)/password-recovery/page.tsx
+++ app/(auth)/password-recovery/page.tsx
@@ -9,11 +9,14 @@ import Link from "next/link";
 export default function page() {
   return (
     <AuthLayout>
-      <Card className="max-w-93.75 w-full h-full max-h-158 rounded-none md:rounded-md bg-transparent md:bg-secondary-light/90 border-0 md:backdrop-blur-sm lg:p-8 lg:min-w-107 ">
-        <div className="flex flex-col items-center gap-4 p-4">
-          <SecurityShield />
+      <Card className="max-w-93.75 w-full h-full max-h-158 rounded-none md:rounded-md bg-transparent md:bg-secondary-light/90 border-0 md:backdrop-blur-sm lg:p-8 lg:min-w-107 md:border-t-5 border-primary">
+        <div className="flex flex-col items-center gap-4 p-4 justify-between h-full">
           <h1 className="title-4 text-foreground">Esqueceu sua Senha?</h1>
-
+          <div className="w-full grid place-items-center">
+            <div className="animate-scale-pulse">
+              <SecurityShield />
+            </div>
+          </div>
           <PasswordRecoveryForm />
 
           <div className="flex flex-col items-center">
