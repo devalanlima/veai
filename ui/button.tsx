ebf011f3209198a84ac25b346c35fdc6356a6364
AlanLima <dev.alanlima@gmail.com>
2026-01-25 15:10:23 -0300

    style: update interface

--- ui/button.tsx
+++ ui/button.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-base font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive cursor-pointer",
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-base font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring focus-visible:ring-[2px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive cursor-pointer",
   {
     variants: {
       variant: {
@@ -15,7 +15,7 @@ const buttonVariants = cva(
           "border-2 border-primary shadow-xs text-primary hover:text-secondary hover:bg-primary",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-background hover:text-primary",
-        ghost: "hover:bg-secondary text-primary hover:text-primary ",
+        ghost: "hover:bg-secondary text-primary hover:text-primary",
         link: "text-primary-foreground hover:text-primary underline-offset-2 hover:underline decoration-2",
       },
       size: {
@@ -24,14 +24,14 @@ const buttonVariants = cva(
         lg: "h-11 rounded-md px-6 has-[>svg]:px-4",
         icon: "size-9",
         "icon-sm": "size-8",
-        "icon-lg": "size-11",
+        "icon-lg": "size-11 [&_svg:not([class*='size-'])]:size-6",
       },
     },
     defaultVariants: {
       variant: "default",
       size: "default",
     },
-  }
+  },
 );
 
 function Button({
