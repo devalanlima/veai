913f695b19337cb2eb152a18d73a41e23abceca2
AlanLima <dev.alanlima@gmail.com>
2026-01-25 15:14:05 -0300

    refactor: now client is created with supabase/ssr sdk

--- lib/supabase/clients/createClient.ts
+++ lib/supabase/clients/createClient.ts
@@ -1,6 +1,7 @@
-import { createClient } from "@supabase/supabase-js";
+import { Database } from "@/types/supabase/supabase";
+import { createBrowserClient } from "@supabase/ssr";
 
-export const supabase = createClient(
+export const supabase = createBrowserClient<Database>(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
 );
--- lib/supabase/clients/createServerClient.ts
+++ lib/supabase/clients/createServerClient.ts
@@ -1,13 +1,30 @@
-import { createClient } from "@supabase/supabase-js";
+import { Database } from "@/types/supabase/supabase";
+import { createServerClient } from "@supabase/ssr";
+import { cookies } from "next/headers";
 
-export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
-  {
-    auth: {
-      autoRefreshToken: false,
-      persistSession: false,
-      detectSessionInUrl: false,
+export async function createClient() {
+  const cookieStore = await cookies();
+
+  return createServerClient<Database>(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
+    {
+      cookies: {
+        getAll() {
+          return cookieStore.getAll();
+        },
+        setAll(cookiesToSet) {
+          try {
+            cookiesToSet.forEach(({ name, value, options }) =>
+              cookieStore.set(name, value, options),
+            );
+          } catch {
+            // The `setAll` method was called from a Server Component.
+            // This can be ignored if you have middleware refreshing
+            // user sessions.
+          }
+        },
+      },
     },
-  },
-);
+  );
+}
